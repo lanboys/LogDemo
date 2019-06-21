df3212f344acd3c916fe7627153bd03e4c4c67b4
lanboys <lan_bing2013@163.com>
2019-06-21 22:38:07 +0800

    测试

--- src/main/java/com/bing/lan/log/log4j2/Log4j2Log.java
+++ src/main/java/com/bing/lan/log/log4j2/Log4j2Log.java
@@ -1,20 +1,16 @@
-package com.bing.lan.log.log4j;
+package com.bing.lan.log.log4j2;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by 蓝兵 on 2019/6/21.
- * <p>
- * https://www.cnblogs.com/baizhanshi/p/7911123.html
- * <p>
- * 不支持使用占位符，不利于代码阅读
  */
 
-public class Log4jLog {
+public class Log4j2Log {
 
     public static void main(String args[]) {
-        Logger logger = LogManager.getLogger(Log4jLog.class);
+        Logger logger = LogManager.getLogger(Log4j2Log.class);
         logger.debug("Debug Level");
         logger.info("Info Level");
         logger.warn("Warn Level");
