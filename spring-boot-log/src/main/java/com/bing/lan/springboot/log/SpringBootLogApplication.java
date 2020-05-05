6d34dd7962da47e68b7411f003f8842f19b95bcf
lanboys <lan_bing2013@163.com>
2020-05-05 23:46:37 +0800

    查看spring-boot中logback状态

--- spring-boot-log/src/main/java/com/bing/lan/springboot/log/SpringBootLogApplication.java
+++ spring-boot-log/src/main/java/com/bing/lan/springboot/log/SpringBootLogApplication.java
@@ -1,15 +1,23 @@
 package com.bing.lan.springboot.log;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 
 @RestController
 @SpringBootApplication
 public class SpringBootLogApplication {
 
     public static void main(String[] args) {
+        //打印 Logback 内部状态, 有了logback.xml文件会自动打印
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
+
         SpringApplication.run(SpringBootLogApplication.class, args);
     }
 
