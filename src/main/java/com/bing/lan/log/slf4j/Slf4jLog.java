8aee6b8e408c3cf6ece1c79da2013687f883a939
lanboys <lan_bing2013@163.com>
2020-05-05 17:31:06 +0800

    测试logback中logger的继承关系
    (cherry picked from commit a449fc1bec1a82c08a184928bcf35f4fb08b0319)

---
+++ src/main/java/com/bing/lan/log/slf4j/Slf4jLog.java
@@ -0,0 +1,29 @@
+package com.bing.lan.log.slf4j;
+
+import com.bing.lan.log.slf4j.cons.ConsoleLog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by 蓝兵 on 2019/6/21.
+ */
+
+public class Slf4jLog {
+
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(Slf4jLog.class);
+        Logger rootLogger = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        System.out.println("main(): -------------------------------------");
+
+        //logger.trace("Trace Level.");
+        //logger.debug("Debug Level.");
+        //logger.info("Info Level.");
+        //logger.warn("Warn Level.");
+        logger.error("Error Level.");
+
+        new ConsoleLog();
+        new FileLog();
+    }
+}
---
+++ src/main/java/com/bing/lan/log/slf4j/cons/ConsoleLog.java
@@ -0,0 +1,16 @@
+package com.bing.lan.log.slf4j.cons;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by 蓝兵 on 2019/6/22.
+ */
+
+public class ConsoleLog {
+
+    public ConsoleLog() {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.warn("Console Log Test");
+    }
+}
