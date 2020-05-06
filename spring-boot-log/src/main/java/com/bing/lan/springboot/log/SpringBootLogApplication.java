cf45e906c62d248d4cdd9ad8d913751dcb17a910
lanboys <lan_bing2013@163.com>
2020-05-06 11:34:45 +0800

    切换spring-boot中日志框架

--- spring-boot-log/src/main/java/com/bing/lan/springboot/log/SpringBootLogApplication.java
+++ spring-boot-log/src/main/java/com/bing/lan/springboot/log/SpringBootLogApplication.java
@@ -1,28 +1,39 @@
 package com.bing.lan.springboot.log;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.boot.context.logging.LoggingApplicationListener;
+import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
-
+/**
+ * 在 {@link LoggingApplicationListener#onApplicationStartingEvent(ApplicationStartingEvent)}
+ * {@link LoggingSystem#get(ClassLoader, String)}
+ * 中确定使用什么日志框架
+ */
 @RestController
 @SpringBootApplication
 public class SpringBootLogApplication {
 
+    private static Logger slf4jLogger = LoggerFactory.getLogger(SpringBootLogApplication.class);
+    private static java.util.logging.Logger julLogger = java.util.logging.Logger.getLogger(SpringBootLogApplication.class.getName());
+
     public static void main(String[] args) {
         //打印 Logback 内部状态, 有了logback.xml文件会自动打印
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        StatusPrinter.print(lc);
+        //LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        //StatusPrinter.print(lc);
 
         SpringApplication.run(SpringBootLogApplication.class, args);
     }
 
     @RequestMapping
     public String hello() {
+        slf4jLogger.info(" >>>>> slf4jLogger >>>>> hello spring-boot logger");
+        julLogger.info(" >>>>> julLogger >>>>> hello spring-boot logger");
         return "hello spring-boot logger";
     }
 }
